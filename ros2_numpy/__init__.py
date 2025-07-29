d8cfcd8849b8c75ba36c84b2d55c1223200f63d4
harumo11 <s_harumo@hotmail.co.jp>
2025-07-29 15:51:06 +0900

    Added to use as a package

---
+++ ros2_numpy/__init__.py
@@ -0,0 +1,6 @@
+#export just numify, msgify, pilify
+from ros2_numpy.image import numpify
+from ros2_numpy.image import msgify
+from ros2_numpy.image import pilify
+
+__all__ = ['numpify', 'msgify', 'pilify']
